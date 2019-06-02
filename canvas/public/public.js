5090981d22205b805706296fa2933753c92ac484
godzhang <zhangqijilindaxue@126.com>
2019-06-02 12:17:40 +0800

    2019-6-1

--- canvas/public/public.js
+++ canvas/public/public.js
@@ -17,3 +17,36 @@ class Particle {
 
   update () {}
 }
+
+function drawGrid (color, stepx, stepy) {
+  ctx.strokeStyle = color
+  ctx.lineWidth = 0.5
+
+  for (let i = stepx + 0.5; i < ctx.canvas.width; i += stepx) {
+    ctx.beginPath()
+    ctx.moveTo(i, 0)
+    ctx.lineTo(i, ctx.canvas.height)
+    ctx.stroke()
+  }
+  for (let i = stepy + 0.5; i < ctx.canvas.height; i += stepy) {
+    ctx.beginPath()
+    ctx.moveTo(0, i)
+    ctx.lineTo(ctx.canvas.width, i)
+    ctx.stroke()
+  }
+}
+
+function rect(x, y, w, h, direction) {
+  if (direction) {
+    ctx.moveTo(x, y)
+    ctx.lineTo(x, y + h)
+    ctx.lineTo(x + w, y + h)
+    ctx.lineTo(x + w, y)
+  } else {
+    ctx.moveTo(x, y)
+    ctx.lineTo(x + w, y)
+    ctx.lineTo(x + w, y + h)
+    ctx.lineTo(x, y + h)
+  }
+  ctx.closePath()
+}
