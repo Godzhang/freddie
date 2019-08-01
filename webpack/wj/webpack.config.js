19e7a283235c9e4549a3f9874507ee735fac9335
godzhang <zhangqijilindaxue@126.com>
2019-08-01 17:43:10 +0800

    2019-8-1

--- webpack/wj/webpack.config.js
+++ webpack/wj/webpack.config.js
@@ -1,10 +1,29 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    app: "./src/index.js",
+    search: "./src/search.js"
+  },
   output: {
     path: path.join(__dirname, "dist"),
-    filename: "bundle.js"
+    filename: "[name].js"
   },
-  mode: "production"
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "less-loader"]
+      }
+    ]
+  }
 };
