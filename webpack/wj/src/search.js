ba4d79a26aba1b15e4b27d0173eff004bd4c74bf
godzhang <zhangqijilindaxue@126.com>
2019-08-01 21:54:14 +0800

    2019-8-1 night

--- webpack/wj/src/search.js
+++ webpack/wj/src/search.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDom from "react-dom";
+import you from "./images/China.png";
 import "./search.less";
 
 class Search extends React.Component {
   render() {
-    return <div className="search-text">search text</div>;
+    return (
+      <div className="search-text">
+        search text
+        <img src={you} />
+      </div>
+    );
   }
 }
 
