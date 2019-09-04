44c6505aac0180843b21bbb8c5e0544d28882c0a
张奇 <zhangqijsj@xinhua.org>
2019-09-04 18:23:36 +0800

    2019-9-4 evening

--- node/test/index.js
+++ node/test/index.js
@@ -6,6 +6,30 @@ const url = require("url");
 const net = require("net");
 const readline = require("readline");
 const crypto = require("crypto");
+const zlib = require("zlib");
 
-// const fileName = path.resolve(__dirname, "test.txt");
-// const readStream = fs.createReadStream(fileName);
+const filePath = "./index.html";
+
+let server = http.createServer((req, res) => {
+  let acceptEncoding = req.headers["accept-encoding"];
+  let gzip;
+
+  if (acceptEncoding.indexOf("gzip") !== -1) {
+    // gzip = zlib.createGzip();
+
+    res.writeHead(200, {
+      "Content-Encoding": "gzip"
+    });
+    // fs.createReadStream(filePath)
+    //   .pipe(gzip)
+    //   .pipe(res);
+    console.log(filePath);
+    let zb = zlib.gzipSync(filePath);
+    console.log(zb);
+    res.end(zb);
+  } else {
+    res.end(responseText);
+    // fs.createReadStream(filePath).pipe(res);
+  }
+});
+server.listen(3000);
