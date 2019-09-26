87df63ac30fcc7434d2212f6d2a10235fdb6a256
张奇 <zhangqijsj@xinhua.org>
2019-09-26 18:00:00 +0800

    2019-9-26 evening

---
+++ fuben/js/defer.js
@@ -0,0 +1,5 @@
+console.log('I"m deferd');
+for (let i = 0; i < 10000000; i++) {
+  new Date();
+}
+console.log("defer script end");
---
+++ fuben/js/normal.js
@@ -0,0 +1,5 @@
+console.log("normal script begin");
+for (let i = 0; i < 10000000; i++) {
+  new Date();
+}
+console.log("normal script end");
