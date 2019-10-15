88a9bfeae21cfd69c1832f6fda1998e3972e5866
张奇 <zhangqijsj@xinhua.org>
2019-10-15 18:00:03 +0800

    2019-10-15 evening

--- vue/v2/src/page/Test.vue
+++ vue/v2/src/page/Test.vue
@@ -2,99 +2,85 @@
  * @Author: 张奇
  * @Date: 2019-10-14 23:21:48
  * @LastEditors: 张奇
- * @LastEditTime: 2019-10-15 00:13:41
+ * @LastEditTime: 2019-10-15 15:34:18
  * @Description: file content
  -->
 <template>
-  <div id="wrapper">
+  <div ref="scroll" id="wrapper">
     <ul>
-      <li style="height: 100px;" v-for="i in 30" :key="i">{{ i }}</li>
+      <li
+        class="item"
+        style="height: 150px;"
+        v-for="i in 20"
+        :key="i"
+        @click="onClick()"
+        @tap="onTap()"
+      >{{ i }}</li>
+      <li ref="sticky" class="item" style="height: 150px;background: #f00;" :top="20">21</li>
+      <li
+        class="item"
+        style="height: 150px;"
+        v-for="i in 20"
+        :key="i + 21"
+        @click="onClick()"
+        @tap="onTap()"
+      >{{ i + 21 }}</li>
     </ul>
   </div>
 </template>
 <script>
 import BScroll from "better-scroll";
 import Swiper from "swiper";
-import IScroll from "iscroll";
+import IScroll from "iscroll/build/iscroll-probe";
+import enableSticky from "../common/iscroll-sticky";
+enableSticky(IScroll);
 
 export default {
   data() {
     return {
-      swiper: null,
-      myScroll: null
+      scroll: null
     };
   },
   mounted() {
-    this.myScroll = new IScroll("#wrapper");
+    const el = this.$refs.scroll;
+    this.scroll = new IScroll("#wrapper", {
+      mouseWheel: true,
+      probeType: 3,
+      snap: ".item",
+      scrollbars: true,
+      shrinkScrollbars: "scale",
+      tap: true,
+      click: true
+    });
+    this.scroll.on("scroll", e => {
+      console.log(this.scroll.y);
+    });
+    const stickyEl = this.$refs.sticky;
+    this.scroll.enableStickyHeaders([
+      {
+        el: stickyEl,
+        top: 400
+      }
+    ]);
   },
-  methods: {}
+  methods: {
+    onTap() {
+      console.log("tap");
+    },
+    onClick() {
+      console.log("click");
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
 #wrapper {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
+  // position: absolute;
+  // top: 0;
+  // left: 0;
+  // width: 100%;
+  position: relative;
+  height: 100%;
   overflow: hidden;
 }
-.container {
-  position: relative;
-  width: 100vw;
-  height: 100vh;
-  overflow-x: hidden;
-  overflow-y: auto;
-  -webkit-perspective: 300px;
-  perspective: 300px;
-}
-.content {
-  position: relative;
-  width: 100vw;
-  height: 100vh;
-  -webkit-transform-style: preserve-3d;
-  transform-style: preserve-3d;
-}
-.item {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  // height: 50vh;
-  background: linear-gradient(#f00, #08c);
-  color: #fff;
-  font-size: 30px;
-}
-.item-1 {
-  transform: translateY(150px) translateZ(-300px) scale(2);
-  // transform: translateZ(-900px) scale(4);
-}
-.item-2,
-.item-3 {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  // transform: translateZ(-300px) scale(2);
-  // transform: translateZ(-1500px) scale(6);
-  background: none;
-}
-// .swiper-container,
-// .swiper-slide {
-//   position: relative;
-//   width: 100vw;
-//   height: 100vh;
-// }
-// .swiper-slide {
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-// }
-// .swiper-bg {
-//   position: absolute;
-//   top: 0;
-//   left: 0;
-//   width: 100vw;
-//   height: 5000px;
-//   background: linear-gradient(#f00, #08c);
-// }
 </style>
