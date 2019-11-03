ec43bf20c1a96d4a913f191a08f9f971be9de4a0
godzhang <zhangqijilindaxue@126.com>
2019-11-03 19:00:56 +0800

    2019-11-3 evening

--- design-pattern/src/index.js
+++ design-pattern/src/index.js
@@ -1,25 +1,69 @@
-class jQuery {
-  constructor(selector) {
-    let slice = Array.prototype.slice;
-    let dom = slice.call(document.querySelectorAll(selector));
-    let len = dom ? dom.length : 0;
-    for (let i = 0; i < len; i++) {
-      this[i] = dom[i];
-    }
-    this.length = len;
-    this.selector = selector || "";
-  }
+// class jQuery {
+//   constructor(selector) {
+//     let slice = Array.prototype.slice;
+//     let dom = slice.call(document.querySelectorAll(selector));
+//     let len = dom ? dom.length : 0;
+//     for (let i = 0; i < len; i++) {
+//       this[i] = dom[i];
+//     }
+//     this.length = len;
+//     this.selector = selector || "";
+//   }
 
-  append(node) {}
+//   append(node) {}
 
-  addClass(name) {}
+//   addClass(name) {}
 
-  html(data) {}
+//   html(data) {}
+// }
+
+// window.$ = function(selector) {
+//   // 工厂模式
+//   return new jQuery(selector);
+// };
+// console.log($("p"));
+// console.log($("p").addClass);
+
+// class Car {
+//   constructor(number, name) {
+//     this.number = number;
+//     this.name = name;
+//   }
+// }
+
+// class Kuaiche extends Car {
+//   constructor(number, name, price) {
+//     super(number, name);
+//     this.price = 1;
+//   }
+// }
+
+// class Zhuanche extends Car {
+//   constructor(number, name, price) {
+//     super(number, name);
+//     this.price = 2;
+//   }
+// }
+
+// class Trip {
+//   constructor(car) {
+//     this.car = car;
+//   }
+//   start() {
+//     console.log(
+//       `行程开始，名称： ${this.car.name}， 车牌号： ${this.car.number}`
+//     );
+//   }
+//   end() {
+//     console.log(`行程结束， 价格：${this.car.price * 5}`);
+//   }
+// }
+
+// let car = new Kuaiche(100, "雪铁龙");
+// let trip = new Trip(car);
+// trip.start();
+// trip.end();
+
+class Park {
+  constructor() {}
 }
-
-window.$ = function(selector) {
-  // 工厂模式
-  return new jQuery(selector);
-};
-console.log($("p"));
-console.log($("p").addClass);
