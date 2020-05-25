d7ac56c054f127609f6ce95311ce87cf23812448
godzhang <zhangqijilindaxue@126.com>
2020-05-26 07:58:29 +0800

    2020-5-26 morning

--- react/react-demo/src/index.js
+++ react/react-demo/src/index.js
@@ -2,71 +2,113 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./index.css";
-// import App from "./App";
+import { createStore } from "redux";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>App</h1>
-        <ul>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/inbox">Inbox</Link>
-          </li>
-          <li>
-            <Link to="/inbox/messages/30">messages</Link>
-          </li>
-        </ul>
-        {this.props.children}
-      </div>
-    );
-  }
-}
-class About extends React.Component {
-  render() {
-    return <h3>About</h3>;
-  }
-}
-class Inbox extends React.Component {
-  render() {
-    return (
-      <div>
-        <h2>Inbox</h2>
-        {this.props.children || "Welcome to your Inbox"}
-      </div>
-    );
-  }
-}
-class Message extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return <h3>Message {this.props.params.id}</h3>;
-  }
-}
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>App</h1>
+//         <ul>
+//           <li>
+//             <Link to="/about">About</Link>
+//           </li>
+//           <li>
+//             <Link to="/inbox">Inbox</Link>
+//           </li>
+//           <li>
+//             <Link to="/inbox/messages/30">messages</Link>
+//           </li>
+//         </ul>
 
-ReactDOM.render(
-  <Router>
-    <Route
-      path="/"
-      component={() => (
-        <App>
-          <Route exact path="/about" component={About} />
-          <Route
-            path="/inbox"
-            component={() => (
-              <Inbox>
-                <Route path="/messages/:id" component={Message} />
-              </Inbox>
-            )}
-          />
-        </App>
-      )}
-    ></Route>
-  </Router>,
-  document.getElementById("root")
-);
+//         <Route exact path="/about" component={About} />
+//         <Route path="/inbox" component={Inbox}></Route>
+//       </div>
+//     );
+//   }
+// }
+// class About extends React.Component {
+//   render() {
+//     return <h3>About</h3>;
+//   }
+// }
+// class Inbox extends React.Component {
+//   render() {
+//     console.log(this.props);
+//     return (
+//       <div>
+//         <h2>Inbox</h2>
+//         <Route
+//           path={`${this.props.match.url}/messages/:id`}
+//           component={Message}
+//         />
+//       </div>
+//     );
+//   }
+// }
+// class Message extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.divRef = React.createRef();
+//   }
+//   linkTo() {
+//     console.log(this.props);
+//     console.log(this.divRef.current);
+//     this.props.history.push("/about", { a: "b" });
+//   }
+//   render() {
+//     console.log(this.props);
+//     return (
+//       <h3>
+//         Message{" "}
+//         <button ref={this.divRef} onClick={this.linkTo.bind(this)}>
+//           {this.props.match.params.id}
+//         </button>
+//       </h3>
+//     );
+//   }
+// }
+
+// ReactDOM.render(
+//   <Router>
+//     <Route path="/" component={App}></Route>
+//   </Router>,
+//   document.getElementById("root")
+// );
+
+const Counter = ({ value, onIncrement, onDecrement }) => {
+  return (
+    <div>
+      <h1>{value}</h1>
+      <button onClick={onIncrement}>+</button>
+      <button onClick={onDecrement}>-</button>
+    </div>
+  );
+};
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+const render = () => {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => store.dispatch({ type: "INCREMENT" })}
+      onDecrement={() => store.dispatch({ type: "DECREMENT" })}
+    />,
+    document.getElementById("root")
+  );
+};
+
+render();
+store.subscribe(render);
