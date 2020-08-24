edcdf960c5345a018d3a8a229dc05c12a97fc0f5
godzhang <zhangqijilindaxue@126.com>
2020-08-25 00:02:22 +0800

    2020-8-25 night

--- vue/v2/src/page/Home.vue
+++ vue/v2/src/page/Home.vue
@@ -1,75 +1,38 @@
 <template>
   <div id="wrap">
-    <button @click="onClick">click</button>
+    <div class="box">
+      <RollNotification :notifications="notifications" />
+    </div>
   </div>
 </template>
 <script>
-// import Vue from "vue";
-// import SeamLessScroll from "../components/seam-less-scroll.vue";
-import obj from "../utils/util";
-console.log(obj);
+import RollNotification from "../components/roll-notification/RollNotification";
 export default {
   data() {
-    return {};
+    return {
+      notifications: [
+        { text: "重要通知", interval: 5000 },
+        {
+          text:
+            "曼联2-2遭南安普顿绝平曼联2-2遭南安普顿绝平曼联2-2遭南安普顿绝平",
+          interval: 5000
+        },
+        {
+          text: "曼联2-2遭南安普顿绝平",
+          interval: 10000
+        }
+      ]
+    };
   },
   mounted() {},
   methods: {
-    onClick() {
-      // this.$toast.show("abc", 2000);
-      this.$msgBox
-        .showMsgBox({
-          title: "提示",
-          content: "确定要删除吗？"
-        })
-        .then(async val => {
-          console.log("确认");
-        })
-        .catch(err => {
-          console.log("取消");
-        });
-    }
+    onClick() {}
   },
-  components: {}
+  components: { RollNotification }
 };
 </script>
 <style lang="scss" scoped>
-.wrap {
-  display: flex;
-  width: 100%;
-  height: 100%;
-  .sidebar-container {
-    position: relative;
-    width: 200px;
-    flex: 0 0 200px;
-    height: 100%;
-    transition: all 0.3s ease;
-    .sidebar {
-      width: 100%;
-      height: 100%;
-      background: #08c;
-      overflow: hidden;
-    }
-    .arrow {
-      position: absolute;
-      top: 50%;
-      right: 0;
-      width: 10px;
-      height: 50px;
-      transform: translateY(-50%);
-      background: #f00;
-    }
-    &.hide {
-      width: 0px;
-      flex: 0 0 0px;
-      .arrow {
-        right: -10px;
-      }
-    }
-  }
-  .content {
-    height: 100%;
-    background: #ff0;
-    flex: auto;
-  }
+.box {
+  width: 400px;
 }
 </style>
