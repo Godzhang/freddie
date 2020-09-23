007f46c5dfb1fa9a54ff5cac56bbcc58168c1b86
张奇 <zhangqijsj@xinhua.org>
2020-09-23 15:13:38 +0800

    2020-9-23 afternoon

--- node/test/index.js
+++ node/test/index.js
@@ -5,6 +5,7 @@ const os = require("os");
 const url = require("url");
 const net = require("net");
 const util = require("util");
+const readline = require("readline");
 
 // const server = net.createServer((socket) => {
 //   console.log("someone connects");
@@ -22,4 +23,96 @@ const util = require("util");
 // console.log(path.resolve("E:\\freddie\\node\\test"));
 // console.log(path.sep);
 
-console.log(os);
+const server = http.createServer((req, res) => {
+  // const method = req.method;
+  // if (method === "POST") {
+  // const fileName = path.resolve(__dirname, "biginput.txt");
+  // const stream = fs.createReadStream(fileName);
+  // stream.pipe(res);
+  // fs.readFile(fileName, "utf-8", (err, data) => {
+  //   res.writeHead(200, { "Content-Type": "text/plain" });
+  //   res.end(data);
+  // });
+  // with no stream
+  // let dataStr = "";
+  // req.on("data", (chunk) => {
+  //   let chunkStr = chunk.toString();
+  //   dataStr += chunkStr;
+  // });
+  // req.on("end", () => {
+  //   const fileName = path.resolve(__dirname, "post.txt");
+  //   fs.writeFile(fileName, dataStr, (err) => {
+  //     res.end("OK");
+  //   });
+  // });
+  // with stream
+  // const fileName = path.resolve(__dirname, "post.txt");
+  // const writeStream = fs.createWriteStream(fileName);
+  // req.pipe(writeStream);
+  // req.on("end", () => {
+  //   res.end("OK");
+  // });
+  // 文件IO操作
+  // with no stream
+  // const readFileName = path.resolve(__dirname, "biginput.txt");
+  // const writeFileName = path.resolve(__dirname, "post.txt");
+  // fs.readFile(readFileName, (err, data) => {
+  //   if (err) {
+  //     console.log(err.message);
+  //     return;
+  //   }
+  //   const dataStr = data.toString();
+  //   fs.writeFile(writeFileName, dataStr, (err) => {
+  //     if (err) {
+  //       console.log(err.message);
+  //       return;
+  //     }
+  //     console.log("拷贝成功");
+  //     res.end("OK");
+  //   });
+  // });
+  // with stream
+  // const readFileName = path.resolve(__dirname, "biginput.txt");
+  // const writeFileName = path.resolve(__dirname, "post.txt");
+  // const readStream = fs.createReadStream(readFileName);
+  // const writeStream = fs.createWriteStream(writeFileName);
+  // readStream.pipe(writeStream);
+  // readStream.on("end", () => {
+  //   res.end("OK");
+  // });
+  // readline
+  // const filename = path.resolve(__dirname, "input.txt");
+  // const readStream = fs.createReadStream(filename);
+  // const rl = readline.createInterface({
+  //   input: readStream,
+  // });
+  // rl.on("line", (lineData) => {
+  //   console.log(lineData);
+  //   console.log("------ this line read ------");
+  // });
+  // rl.on("close", () => {
+  //   console.log("readline end");
+  //   res.end("OK");
+  // });
+  // }
+});
+server.listen(9000, () => {
+  console.log("server is running at port 9000");
+});
+// const Readable = require("stream").Readable;
+
+// const rs = new Readable();
+// rs.push("xuexi");
+// rs.push("qiaoyafang");
+// rs.push(null);
+
+// rs.pipe(process.stdout);
+
+// const Writeable = require("stream").Writable;
+
+// const ws = Writeable();
+// ws._write = function (chunk, enc, next) {
+//   console.log(chunk.toString());
+//   next();
+// };
+// process.stdin.pipe(ws);
