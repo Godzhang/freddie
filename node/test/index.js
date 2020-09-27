079a33f3002cb9635d942a28cbaf43dd68e383a7
张奇 <zhangqijsj@xinhua.org>
2020-09-27 16:17:07 +0800

    2020-9-27 afternoon

--- node/test/index.js
+++ node/test/index.js
@@ -7,6 +7,8 @@ const net = require("net");
 const util = require("util");
 const readline = require("readline");
 
+const resolve = (d) => path.resolve(__dirname, d);
+
 // const server = net.createServer((socket) => {
 //   console.log("someone connects");
 // });
@@ -23,82 +25,83 @@ const readline = require("readline");
 // console.log(path.resolve("E:\\freddie\\node\\test"));
 // console.log(path.sep);
 
-const server = http.createServer((req, res) => {
-  // const method = req.method;
-  // if (method === "POST") {
-  // const fileName = path.resolve(__dirname, "biginput.txt");
-  // const stream = fs.createReadStream(fileName);
-  // stream.pipe(res);
-  // fs.readFile(fileName, "utf-8", (err, data) => {
-  //   res.writeHead(200, { "Content-Type": "text/plain" });
-  //   res.end(data);
-  // });
-  // with no stream
-  // let dataStr = "";
-  // req.on("data", (chunk) => {
-  //   let chunkStr = chunk.toString();
-  //   dataStr += chunkStr;
-  // });
-  // req.on("end", () => {
-  //   const fileName = path.resolve(__dirname, "post.txt");
-  //   fs.writeFile(fileName, dataStr, (err) => {
-  //     res.end("OK");
-  //   });
-  // });
-  // with stream
-  // const fileName = path.resolve(__dirname, "post.txt");
-  // const writeStream = fs.createWriteStream(fileName);
-  // req.pipe(writeStream);
-  // req.on("end", () => {
-  //   res.end("OK");
-  // });
-  // 文件IO操作
-  // with no stream
-  // const readFileName = path.resolve(__dirname, "biginput.txt");
-  // const writeFileName = path.resolve(__dirname, "post.txt");
-  // fs.readFile(readFileName, (err, data) => {
-  //   if (err) {
-  //     console.log(err.message);
-  //     return;
-  //   }
-  //   const dataStr = data.toString();
-  //   fs.writeFile(writeFileName, dataStr, (err) => {
-  //     if (err) {
-  //       console.log(err.message);
-  //       return;
-  //     }
-  //     console.log("拷贝成功");
-  //     res.end("OK");
-  //   });
-  // });
-  // with stream
-  // const readFileName = path.resolve(__dirname, "biginput.txt");
-  // const writeFileName = path.resolve(__dirname, "post.txt");
-  // const readStream = fs.createReadStream(readFileName);
-  // const writeStream = fs.createWriteStream(writeFileName);
-  // readStream.pipe(writeStream);
-  // readStream.on("end", () => {
-  //   res.end("OK");
-  // });
-  // readline
-  // const filename = path.resolve(__dirname, "input.txt");
-  // const readStream = fs.createReadStream(filename);
-  // const rl = readline.createInterface({
-  //   input: readStream,
-  // });
-  // rl.on("line", (lineData) => {
-  //   console.log(lineData);
-  //   console.log("------ this line read ------");
-  // });
-  // rl.on("close", () => {
-  //   console.log("readline end");
-  //   res.end("OK");
-  // });
-  // }
-});
-server.listen(9000, () => {
-  console.log("server is running at port 9000");
-});
+// const server = http.createServer((req, res) => {
+// const method = req.method;
+// if (method === "POST") {
+// const fileName = path.resolve(__dirname, "biginput.txt");
+// const stream = fs.createReadStream(fileName);
+// stream.pipe(res);
+// fs.readFile(fileName, "utf-8", (err, data) => {
+//   res.writeHead(200, { "Content-Type": "text/plain" });
+//   res.end(data);
+// });
+// with no stream
+// let dataStr = "";
+// req.on("data", (chunk) => {
+//   let chunkStr = chunk.toString();
+//   dataStr += chunkStr;
+// });
+// req.on("end", () => {
+//   const fileName = path.resolve(__dirname, "post.txt");
+//   fs.writeFile(fileName, dataStr, (err) => {
+//     res.end("OK");
+//   });
+// });
+// with stream
+// const fileName = path.resolve(__dirname, "post.txt");
+// const writeStream = fs.createWriteStream(fileName);
+// req.pipe(writeStream);
+// req.on("end", () => {
+//   res.end("OK");
+// });
+// 文件IO操作
+// with no stream
+// const readFileName = path.resolve(__dirname, "biginput.txt");
+// const writeFileName = path.resolve(__dirname, "post.txt");
+// fs.readFile(readFileName, (err, data) => {
+//   if (err) {
+//     console.log(err.message);
+//     return;
+//   }
+//   const dataStr = data.toString();
+//   fs.writeFile(writeFileName, dataStr, (err) => {
+//     if (err) {
+//       console.log(err.message);
+//       return;
+//     }
+//     console.log("拷贝成功");
+//     res.end("OK");
+//   });
+// });
+// with stream
+// const readFileName = path.resolve(__dirname, "biginput.txt");
+// const writeFileName = path.resolve(__dirname, "post.txt");
+// const readStream = fs.createReadStream(readFileName);
+// const writeStream = fs.createWriteStream(writeFileName);
+// readStream.pipe(writeStream);
+// readStream.on("end", () => {
+//   res.end("OK");
+// });
+// readline
+// const filename = path.resolve(__dirname, "input.txt");
+// const readStream = fs.createReadStream(filename);
+// const rl = readline.createInterface({
+//   input: readStream,
+// });
+// rl.on("line", (lineData) => {
+//   console.log(lineData);
+//   console.log("------ this line read ------");
+// });
+// rl.on("close", () => {
+//   console.log("readline end");
+//   res.end("OK");
+// });
+// }
+// });
+// server.listen(9000, () => {
+//   console.log("server is running at port 9000");
+// });
+
 // const Readable = require("stream").Readable;
 
 // const rs = new Readable();
@@ -116,3 +119,55 @@ server.listen(9000, () => {
 //   next();
 // };
 // process.stdin.pipe(ws);
+
+// Promise.resolve()
+//   .then(function () {
+//     console.log("promise1");
+//   })
+//   .then(function () {
+//     console.log("promise2");
+//   });
+
+// process.nextTick(() => {
+//   console.log("nextTick1");
+//   process.nextTick(() => {
+//     console.log("nextTick2");
+//     process.nextTick(() => {
+//       console.log("nextTick3");
+//       process.nextTick(() => {
+//         console.log("nextTick4");
+//       });
+//     });
+//   });
+// });
+// setImmediate(function () {
+//   console.log("老铁，我是被 setImmediate 派发的");
+// });
+
+// setTimeout(function () {
+//   console.log("老铁，我是被 setTimeout 派发的");
+// }, 0);
+
+// fs.readFile(resolve("test.js"), "utf8", (err, data) => {
+//   setTimeout(function () {
+//     console.log("老铁，我是被 setTimeout 派发的");
+//   }, 0);
+//   setImmediate(function () {
+//     console.log("老铁，我是被 setImmediate 派发的");
+//   });
+// });
+
+setTimeout(() => {
+  console.log("timeout1");
+}, 0);
+
+setTimeout(() => {
+  console.log("timeout2");
+  Promise.resolve().then(function () {
+    console.log("promise1");
+  });
+}, 0);
+
+setTimeout(() => {
+  console.log("timeout3");
+}, 0);
