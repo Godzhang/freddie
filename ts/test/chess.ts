ccd50e7d32217c6eb9dc5574689661442fafbcb9
godzhang <zhangqijilindaxue@126.com>
2020-10-21 23:10:57 +0800

    2020-10-21 night

---
+++ ts/test/chess.ts
@@ -0,0 +1,12 @@
+class Game {}
+// 棋子
+class Piece {}
+// 坐标
+class Position {}
+// 棋子分类
+class King extends Piece {}
+class Queen extends Piece {}
+class Bishop extends Piece {}
+class Knight extends Piece {}
+class Rook extends Piece {}
+class Pawn extends Piece {}
