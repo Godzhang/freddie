c4fb8f0da4dd40b22eb5e19a038d1deba5b65a7d
godzhang <zhangqijilindaxue@126.com>
2020-11-17 23:29:54 +0800

    2020-11-17 night

--- react/react-study/src/App.js
+++ react/react-study/src/App.js
@@ -1,8 +1,47 @@
-import React, { createContext, useContext, useState, useCallback } from "react";
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useCallback,
+  useReducer,
+  forwardRef,
+  useEffect,
+  useRef,
+  useImperativeHandle,
+} from "react";
 import "./App.css";
 
 function App() {
-  return <div>App</div>;
+  const inputEl = useRef(null);
+  console.log(inputEl.current);
+  const plus = () => {
+    const { value, setValue } = inputEl.current;
+    setValue(value + 1);
+  };
+  return (
+    <div>
+      <Child ref={inputEl}></Child>
+      <div>{inputEl.current && inputEl.current.name}</div>
+      <button onClick={() => plus()}>累加子组件value</button>
+    </div>
+  );
 }
+
+const Child = forwardRef((props, ref) => {
+  const inputRef = useRef();
+  const [value, setValue] = useState(0);
+
+  useImperativeHandle(ref, () => ({
+    setValue,
+    value,
+  }));
+
+  return (
+    <>
+      <div>child-value: {value}</div>
+      <input ref={inputRef} />
+    </>
+  );
+});
 
 export default App;
