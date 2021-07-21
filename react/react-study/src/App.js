47bd379f49c7337681a10c93fe04a7f45eddc5b8
张奇 <zhangqijsj@xinhua.org>
2021-07-21 16:09:37 +0800

    react-redux test

--- react/react-study/src/App.js
+++ react/react-study/src/App.js
@@ -1,180 +1,86 @@
-import React, {
-  createContext,
-  useContext,
-  useState,
-  useCallback,
-  useReducer,
-  forwardRef,
-  useEffect,
-  useRef,
-  useImperativeHandle,
-  useMemo,
-} from "react";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+} from "react-router-dom";
 import "./App.css";
 
-// function Example(props) {
-//   const { count } = props;
-//   const handleClick = () => {
-//     setTimeout(() => {
-//       console.log(count);
-//     }, 3000);
-//   };
-//   return (
-//     <div>
-//       <p>{count}</p>
-//       <button onClick={handleClick}>Alert Count</button>
-//     </div>
-//   );
-// }
-
-// class Example2 extends React.Component {
-//   handleClick = () => {
-//     setTimeout(() => {
-//       console.log(this.props.count);
-//     }, 3000);
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <h2>Example2</h2>
-//         <p>{this.props.count}</p>
-//         <button onClick={this.handleClick}>Alert Count</button>
-//       </div>
-//     );
-//   }
-// }
-
-// const Example = () => {
-//   const [count, setCount] = useState(0);
-//   const currentCount = useRef(count);
-//   currentCount.current = count;
-
-//   const handleClick = () => {
-//     setTimeout(() => {
-//       setCount(currentCount.current + 1);
-//       // setCount((prevCount) => prevCount + 1);
-//     }, 3000);
-//   };
-
-//   return (
-//     <div>
-//       <p>{count}</p>
-//       <button onClick={() => setCount(count + 1)}>setCount</button>
-//       <button onClick={handleClick}>delay setCount</button>
-//     </div>
-//   );
-// };
-
-// const Example = () => {
-//   const [count, setCount] = useState(1);
-//   const prevCountRef = useRef(count);
-//   const prevCount = prevCountRef.current;
-//   prevCountRef.current = count;
-
-//   const handleClick = () => {
-//     setCount(prevCount + count);
-//   };
-
-//   return (
-//     <div>
-//       <p>{count}</p>
-//       <button onClick={handleClick}>setCount</button>
-//     </div>
-//   );
-// };
-
-// const Counter = () => {
-//   const [count, setCount] = useState(0);
-
-//   useEffect(() => {
-//     setTimeout(() => {
-//       console.log(`you clicked ${count} times`);
-//     }, 3000);
-//   }, []);
-
-//   return (
-//     <div>
-//       <p>you clicked {count} times</p>
-//       <button onClick={() => setCount(count + 1)}>click me</button>
-//     </div>
-//   );
-// };
-
-// function Example(props) {
-//   const [count, setCount] = useState(0);
-//   const [foo] = useState({});
-
-//   useEffect(() => {
-//     console.log("useEffect");
-//   }, [foo]);
-
-//   const main = useMemo(() => {
-//     console.log("main");
-//     return (
-//       <ul>
-//         <li>1</li>
-//         <li>2</li>
-//         <li>3</li>
-//       </ul>
-//     );
-//   }, [foo]);
-
-//   return (
-//     <div>
-//       <p>{count}</p>
-//       <button onClick={() => setCount(count + 1)}>click</button>
-//       {main}
-//     </div>
-//   );
-// }
-
-const Example = (props) => {
-  function init(initialCount) {
-    console.log(1);
-    return { count: initialCount };
-  }
-
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case "increment":
-        return { count: state.count + 1 };
-      case "decrement":
-        return { count: state.count - 1 };
-      case "reset":
-        return init(action.payload);
-      default:
-        throw new Error();
-    }
-  };
-
-  const [state, dispatch] = useReducer(reducer, props.initialCount, init);
-
+const App = () => {
   return (
-    <div>
-      <p>Count: {state.count}</p>
-      <button
-        onClick={() => dispatch({ type: "reset", payload: props.initialCount })}
-      >
-        Reset
-      </button>
-      <button onClick={() => dispatch({ type: "increment" })}>+</button>
-      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/about" component={About}></Route>
+        <Route path="/topics" component={Topics}></Route>
+        <Route path="/">
+          <Home />
+        </Route>
+        {/* <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route> */}
+      </Switch>
+    </Router>
   );
 };
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className="app">
-        <Example initialCount={1} />
-      </div>
-    );
-  }
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Topics() {
+  const match = useRouteMatch();
+  console.log("match: ", match);
+
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path={`${match.path}/:topicId`}>
+          <Topic />
+        </Route>
+        <Route path={match.path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Topic() {
+  const params = useParams();
+  console.log("params: ", params);
+  return <h3>Requested topic ID: {params.topicId}</h3>;
 }
 
 export default App;
