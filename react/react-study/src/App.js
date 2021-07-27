40d7d34bdaf469a3262edb7763b8a9be1c972652
张奇 <zhangqijsj@xinhua.org>
2021-07-27 16:06:49 +0800

    看完一遍react官方文档

--- react/react-study/src/App.js
+++ react/react-study/src/App.js
@@ -1,46 +1,10 @@
-import React, { createRef, useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
 
 // const LazyComponent = lazy(() => import("./components/LazyComponent"))
 
 const App = () => {
-  return (
-    <div className="app">
-      <Mouse render={(pos) => <ShowPos pos={pos} />}></Mouse>
-    </div>
-  );
-};
-
-const ShowPos = (props) => {
-  const pos = props.pos;
-  return (
-    <p>
-      The current mouse position is ({pos.x}, {pos.y})
-    </p>
-  );
-};
-
-const Mouse = (props) => {
-  const [pos, setPos] = useState({ x: 0, y: 0 });
-
-  const onHandleMouseMove = (e) => {
-    setPos({
-      x: e.clientX,
-      y: e.clientY,
-    });
-  };
-
-  return (
-    <div
-      style={{ width: "100%", height: "100%" }}
-      onMouseMove={onHandleMouseMove}
-    >
-      {props.render(pos)}
-      {/* <p>
-        The current mouse position is ({pos.x}, {pos.y})
-      </p> */}
-    </div>
-  );
+  return <div className="app"></div>;
 };
 
 export default App;
